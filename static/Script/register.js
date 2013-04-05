100809546dfc80f449c2410b53861efaca94e799
joke <alta.second@gmail.com>
2013-04-05 12:49:54 +0900

    빼먹은 파일

---
+++ static/Script/register.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+	$("#addFoodButton").click(function() {
+		$("#restaurantTable").append('<tr><td>이름</td><td><input type="textbox" name="food_name" /></td><td>가격</td><td><input type="textbox" name="food_price" /></td></tr>');
+	});
+
+
+	$('#registerRestaurantForm').submit(function() {
+		data = $('#registerRestaurantForm').serialize();
+
+		$.ajax({
+			type:'POST',
+			url:'/registerRestaurant/',
+			data: data,
+			dataType: "json",
+			success: function(data) {
+				state = data[0].state;
+				if (state == 0) {
+					alert("음식점 등록 성공!");
+					$('#registerRestaurantForm')[0].reset();
+
+				}
+				else {
+					alert("음식점 등록 실패");
+				}
+			},
+			error: function(xhr,err) {
+				alert("readyState: "+xhr.readyState+"\nstatus: "+xhr.status);
+				//alert("responseText: "+xhr.responseText);
+			}
+		});
+		event.preventDefault();
+	});
+});
